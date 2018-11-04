21f5f1519e03eb637266737c22b06143e06e4e3e
Erik Dahl <dahlsailrunner@yahoo.com>
2018-11-04 07:48:13 -0600

    updated to add logging to api

--- SimpleApi/Program.cs
+++ SimpleApi/Program.cs
@@ -1,24 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
 
 namespace SimpleApi
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var name = Assembly.GetExecutingAssembly().GetName();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("IdentityServer4", LogEventLevel.Information)
+                //.Enrich.FromLogContext()
+                //.Enrich.WithMachineName()
+                //.Enrich.WithProperty("Assembly", $"{name.Name}")
+                //.Enrich.WithProperty("Version", $"{name.Version}")
+                .WriteTo.File(new RenderedCompactJsonFormatter(), @"C:\users\edahl\Source\Logs\SimpleApi.json")
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                CreateWebHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseSerilog();
+
+
+        public static void AddCustomContextInfo(IHttpContextAccessor ctx, LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            HttpContext context = ctx.HttpContext;
+            if (context == null)
+            {
+                return;
+            }
+            var userInfo = context.Items[$"serilog-enrichers-aspnetcore-userinfo"] as UserInfo;
+            if (userInfo == null)
+            {
+                var user = context.User.Identity;
+                if (user == null || !user.IsAuthenticated) return;
+                userInfo = new UserInfo
+                {
+                    Name = user.Name,
+                    Claims = context.User.Claims.ToDictionary(x => x.Type, y => y.Value)
+                };
+                context.Items[$"serilog-enrichers-aspnetcore-userinfo"] = userInfo;
+            }
+
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("UserInfo", userInfo, true));
+        }
+    }
+    public class UserInfo
+    {
+        public string Name { get; set; }
+        public Dictionary<string, string> Claims { get; set; }
     }
 }
